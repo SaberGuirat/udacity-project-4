bb82cbf24daf51c0d4c59e0482026ca47a1e2955
SaberGuirat <s.guirat@digital-expert.io>
2022-10-05 13:55:46 +0200

    feat: update and delete enpoints

--- backend/src/helpers/todos.ts
+++ backend/src/helpers/todos.ts
@@ -2,7 +2,7 @@ import { TodosAccess } from './todosAcess'
 // import { AttachmentUtils } from './attachmentUtils'
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
-// import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
 // import * as createError from 'http-errors'
@@ -13,22 +13,55 @@ const logger = createLogger('TodosBusinessLogic')
 const todosAccess = new TodosAccess()
 
 export const getTodosForUser = (userId: string): Promise<TodoItem[]> => {
-  logger.info('Getting todos for specific user')
-  return todosAccess.getTodosForUser(userId)
+  try {
+    logger.info(`Getting todos for specific user with id ${userId}`)
+    return todosAccess.getTodosForUser(userId)
+  } catch (error) {
+    logger.error(error.message)
+  }
 }
 export const createTodo = async (
   createTodoRequest: CreateTodoRequest,
   userId: string
 ): Promise<TodoItem> => {
-  logger.info('Creating a new todo')
-  const todoId: string = uuid.v4()
-  const newItem: TodoItem = {
-    userId,
-    todoId,
-    createdAt: new Date().toISOString(),
-    done: false,
-    ...createTodoRequest
-  }
+  try {
+    logger.info(`Creating a new todo for user with id ${userId}`)
+    const todoId: string = uuid.v4()
+    const newItem: TodoItem = {
+      userId,
+      todoId,
+      createdAt: new Date().toISOString(),
+      done: false,
+      ...createTodoRequest
+    }
 
-  return await todosAccess.createTodo(newItem)
+    return await todosAccess.createTodo(newItem)
+  } catch (error) {
+    logger.error(error.message)
+  }
+}
+
+export const updateTodo = async (
+  todoId: string,
+  userId: string,
+  updateTodoRequest: UpdateTodoRequest
+): Promise<void> => {
+  try {
+    logger.info(`Updating a todo with id ${todoId} for user with id ${userId}`)
+    await todosAccess.updateTodo(todoId, userId, updateTodoRequest)
+  } catch (error) {
+    logger.error(error.message)
+  }
+}
+
+export const deleteTodo = async (
+  todoId: string,
+  userId: string
+): Promise<void> => {
+  try {
+    logger.info(`Deleting a todo with id ${todoId} for user with id ${userId}`)
+    return await todosAccess.deleteTodo(todoId, userId)
+  } catch (error) {
+    logger.error(error.message)
+  }
 }
--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -3,7 +3,7 @@ import * as AWSXRay from 'aws-xray-sdk'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { createLogger } from '../utils/logger'
 import { TodoItem } from '../models/TodoItem'
-// import { TodoUpdate } from '../models/TodoUpdate'
+import { TodoUpdate } from '../models/TodoUpdate'
 
 const XAWS = AWSXRay.captureAWS(AWS)
 
@@ -19,7 +19,7 @@ export class TodosAccess {
   ) {}
   async getTodosForUser(userId: string): Promise<TodoItem[]> {
     try {
-      logger.info('Getting todos for specific user')
+      logger.info(`Getting todos for specific user with id ${userId}`)
       const result = await this.docClient
         .query({
           TableName: this.todosTable,
@@ -39,7 +39,7 @@ export class TodosAccess {
 
   async createTodo(todoItem: TodoItem): Promise<TodoItem> {
     try {
-      logger.info('Creating a new todo')
+      logger.info(`Creating a new todo for user with id ${todoItem.userId}`)
       await this.docClient
         .put({
           TableName: this.todosTable,
@@ -48,6 +48,56 @@ export class TodosAccess {
         .promise()
 
       return todoItem
+    } catch (error) {
+      logger.error(error.message)
+    }
+  }
+
+  async updateTodo(
+    todoId: string,
+    userId: string,
+    todoUpdate: TodoUpdate
+  ): Promise<void> {
+    try {
+      logger.info(
+        `Updating a todo with id ${todoId} for user with id ${userId}`
+      )
+      await this.docClient
+        .update({
+          TableName: this.todosTable,
+          Key: {
+            todoId,
+            userId
+          },
+          UpdateExpression:
+            'set #name = :name, dueDate = :dueDate, done = :done',
+          ExpressionAttributeValues: {
+            ':name': todoUpdate.name,
+            ':dueDate': todoUpdate.dueDate,
+            ':done': todoUpdate.done
+          },
+          ExpressionAttributeNames: {
+            '#name': 'name'
+          }
+        })
+        .promise()
+    } catch (error) {
+      logger.error(error.message)
+    }
+  }
+
+  async deleteTodo(todoId: string, userId: string): Promise<void> {
+    try {
+      logger.info(`Deleting a todo with id ${todoId} for user with id ${userId}`)
+      await this.docClient
+        .delete({
+          TableName: this.todosTable,
+          Key: {
+            todoId,
+            userId
+          }
+        })
+        .promise()
     } catch (error) {
       logger.error(error.message)
     }
